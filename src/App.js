f5cc0f64bbd344b841afda4930c510ffa0d0edb8
羅云謙 <yunchienlo@AIBO.local>
2021-11-10 16:56:09 +0800

    [Dev] 建立建立wireframe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import "./style/index.scss";
 function App() {
   return (
     <div className="App">
-      
+      <div className="calculator">
+        <div className="calculator__input"></div>
+        <div className="calculator__body">
+          <div className="calculator__btn">7</div>
+          <div className="calculator__btn">8</div>
+          <div className="calculator__btn">9</div>
+          <div className="calculator__btn">DEL</div>
+          <div className="calculator__btn">4</div>
+          <div className="calculator__btn">5</div>
+          <div className="calculator__btn">6</div>
+          <div className="calculator__btn">+</div>
+          <div className="calculator__btn">1</div>
+          <div className="calculator__btn">2</div>
+          <div className="calculator__btn">3</div>
+          <div className="calculator__btn">-</div>
+          <div className="calculator__btn">.</div>
+          <div className="calculator__btn">0</div>
+          <div className="calculator__btn">/</div>
+          <div className="calculator__btn">*</div>
+          <div className="calculator__btn calculator--large">RESET</div>
+          <div className="calculator__btn calculator--large">=</div>
+        </div>
+
+      </div>
     </div>
   );
 }
